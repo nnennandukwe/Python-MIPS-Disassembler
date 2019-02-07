12494c090b2e383c641fd65d511fc511ee7ec803
Nnenna Ndukwe <nnenna.s.ndukwe@gmail.com>
2019-02-06 23:36:07 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+# MIPS DISASSEMBLER 
+
+# Input will be 32-bit instructions that a compiler or assembler produces
+# program figures out what the original source instructions were that creatd those 32-bit machine instructions
+# and outputs them.
+
+# All Instructions that Must be able to be disassembld are:
+# add, sub, and, or, slt, lw, sw, beq, bne
